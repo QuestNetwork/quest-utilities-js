d5509e2eb5ccc3d53f1001e9de76d8772519da0f
StationedInTheField <ask@stationedinthefield.com>
2020-10-08 03:39:58 -0700

    heartbeat needed this

--- utilities.js
+++ utilities.js
@@ -8,10 +8,16 @@ export class Utilities {
     this.engine = new Engine();
   }
 
-
+  delay(t, val = "") {
+    return new Promise(function(resolve) {
+        setTimeout(function() {
+            resolve(val);
+        }, t);
+    });
+ }
 
   inArray(array,value) {
-    if(typeof array['indexOf'] != 'undefined') { 
+    if(typeof array['indexOf'] != 'undefined') {
       return array.indexOf(value) > -1;
     }
     return false;
